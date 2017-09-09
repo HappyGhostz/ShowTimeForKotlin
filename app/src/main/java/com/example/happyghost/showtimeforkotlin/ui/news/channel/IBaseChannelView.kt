635ee8d27eb3c90caecb546b29ba07c57af1bbe1
HappyGhostz <zcphappyghost@163.com>
2017-09-09 10:57:45 +0800

    添加因包变化遗漏的类

---
+++ app/src/main/java/com/example/happyghost/showtimeforkotlin/ui/news/channel/IBaseChannelView.kt
@@ -0,0 +1,17 @@
+package com.example.happyghost.showtimeforkotlin.ui.news.channel
+
+import com.example.happyghost.showtimeforkotlin.loacaldao.NewsTypeInfo
+
+/**
+ * @author Zhao Chenping
+ * @creat 2017/9/4.
+ * @description
+ */
+interface IBaseChannelView {
+    /**
+     * 显示数据
+     * @param checkList     选中栏目
+     * @param uncheckList   未选中栏目
+     */
+     fun loadData(checkList: ArrayList<NewsTypeInfo>, uncheckList: ArrayList<NewsTypeInfo>)
+}
---
+++ app/src/main/java/com/example/happyghost/showtimeforkotlin/ui/news/newslist/INewsListView.kt
@@ -0,0 +1,18 @@
+package com.example.happyghost.showtimeforkotlin.ui.news.newslist
+
+import com.example.happyghost.showtimeforkotlin.bean.NewsInfo
+import com.example.happyghost.showtimeforkotlin.bean.NewsMultiItem
+
+/**
+ * @author Zhao Chenping
+ * @creat 2017/9/5.
+ * @description
+ */
+interface INewsListView:ILoadDataView<List<NewsMultiItem>>{
+    /**
+     * 加载广告数据
+     * @param newsBean 新闻
+     */
+    fun loadAdData(newsBean: NewsInfo)
+
+}
